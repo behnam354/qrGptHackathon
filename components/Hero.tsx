fee74e61ec90010191b98b872acf6369cf5f00ed
Behnam Shahbazi <behnam.shahbazi40@gmail.com>
2024-11-21 19:09:21 -0800

    remove unused heroImages

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,8 +3,6 @@
 import Image from 'next/image';
 import NavLink from './NavLink';
 
-let heroImages = ['/1.png', '/6.png', '/3.png', '/4.png', '/5.png', '/2.png'];
-
 export default function Hero() {
   return (
     <section>
